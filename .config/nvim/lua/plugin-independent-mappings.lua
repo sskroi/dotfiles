bd571a3f2efa926a4391581e06c108e9bf3594a8
Andrei E <sskroi200ok@gmail.com>
2024-06-22 03:23:35 +0300

    Nvim: updated mappings: added multiple lines moving, deleted J/K mappings in visual mode

--- .config/nvim/lua/plugin-independent-mappings.lua
+++ .config/nvim/lua/plugin-independent-mappings.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 
-map({ "n", "x" }, "J", "5<Down>", { desc = "Jump 5 lines down" })
-map({ "n", "x" }, "K", "5<Up>", { desc = "Jump 5 lines up" })
+map({ "n" }, "J", "5<Down>", { desc = "Jump 5 lines down" })
+map({ "n" }, "K", "5<Up>", { desc = "Jump 5 lines up" })
 
 map({ "n", "i", "x" }, "<C-s>", "<Cmd>w<CR>", { desc = "Write current buffer" })
 map({ "n" }, "<C-a>", "ggVG", { desc = "Select all" })
@@ -13,3 +13,7 @@ map("n", "<C-k>", "<C-w>k", { desc = "Focus on top-side window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Focus on right-side window" })
 
 map("n", "<Esc>", "<Cmd>nohlsearch<CR>", { desc = "Stop highlighting search", silent = true })
+
+-- Lines moving in visual mode
+map("x", "<C-j>", ":move '>+1<CR>gv")
+map("x", "<C-k>", ":move '<-2<CR>gv")
