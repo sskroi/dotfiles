b7b3ec3da1e1979c0858c73d76eb086511898d63
Andrei E <sskroi200ok@gmail.com>
2024-06-27 21:39:46 +0300

    Waybar: updated theme change script

--- .config/waybar/change_config.sh
+++ .config/waybar/change_config.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <folder>"
-    exit 1
-fi
-
-ln -sf $1/config.jsonc ./config.jsonc
-ln -sf $1/style.css ./style.css
+theme=${1}
+cd ${HOME}/.config/waybar/${theme}
+stow .
 
 killall waybar && nohup waybar 1>/dev/null 2>/dev/null &
 
